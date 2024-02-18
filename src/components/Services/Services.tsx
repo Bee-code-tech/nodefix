64f1c92d1cdb8a4334085c026bd43180a10db19b
Jumah Muhamad <jumahtech@gmail.com>
2024-02-18 23:21:57 +0100

    addaed the services section

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -1,24 +1,44 @@
 // import React from "react";
-import "./Services.css"
-import img1 from "../../assets/cryptocurrency-3d-illustration-free-png.webp";
-// import img2 from "../../assets/get-started-how-do-crypto-exchanges-work.png";
+import "./Services.css";
+import { faBolt } from "@fortawesome/free-solid-svg-icons";
+import { faShieldAlt } from "@fortawesome/free-solid-svg-icons";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Services = () => {
   return (
     <div>
-      <div className="container">
-        <div className="row">
-          <div className="col-12 col-sm-12 col-md-4 col-lg-4">
-            <img className="w-100 bg-white" src={img1} alt="" />
-            <p className="text-center text-white">Fast-migration</p>
-          </div>
-          <div className="col-12 col-sm-12 col-md-4 col-lg-4">
-            <img className="w-100 bg-white" src={img1} alt="" />
-            <p className="text-center text-white">Fast-migration</p>
-          </div>
-          <div className="col-12 col-sm-12 col-md-4 col-lg-4">
-            <img className="w-100 bg-white" src={img1} alt="" />
-            <p className="text-center text-white">Fast-migration</p>
+      <div className="blue p-4">
+        <div className="container mt-5">
+          <h3 className="text-white fw-bold fs-1">Services</h3>
+          <div className="row d-flex">
+            <div className="icon-div col-12 col-sm-12 col-md-4 col-lg-4 text-center p-5 mb-3">
+              <FontAwesomeIcon className="bg-primary p-3 icons" icon={faBolt} />
+              <p className="fw-bold text-white">Quick Action</p>
+              <p className="text-white">
+                "Explore the World of Cryptocurrency Trading with Us - Take
+                Action Now!"
+              </p>
+            </div>
+            <div className="icon-div col-12 col-sm-12 col-md-4 col-lg-4 text-center p-5 mb-3">
+              <FontAwesomeIcon
+                className="bg-success p-3 icons"
+                icon={faShieldAlt}
+              />
+              <p className="fw-bold text-white">Safe Transaction</p>
+              <p className="text-white">
+                "Secure Your Transactions Today for Peace of Mind and
+                Confidence!"
+              </p>
+            </div>
+            <div className="icon-div col-12 col-sm-12 col-md-4 col-lg-4 text-center p-5 mb-3">
+              <FontAwesomeIcon className="bg-danger p-3 icons" icon={faLock} />
+              <p className="fw-bold text-white">Encrypted Credentials</p>
+              <p className="text-white">
+                "Safeguard Your Data with Cutting-edge Encryption Technology -
+                Protect Your Credentials Today!"
+              </p>
+            </div>
           </div>
         </div>
       </div>
