a98346bfa1ac9564ec40645d011563853ded997a
Jumah Muhamad <jumahtech@gmail.com>
2024-02-19 00:27:11 +0100

    edits

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -24,28 +24,36 @@ const Contact = () => {
               Contact
             </button>
           </div>
-          <div className="col-12 col-sm-12 col-md-6 col-lg-6 mb-4 purple mt-3">
-            <h3 className="explore-h3 text-white fs-4 fw-light">
-              Encrypted Credentials
-            </h3>
-            <p className="explore-p fs-5 fw-light text-white mt-3">
-              Our platform employs cutting-edge encryption techniques to protect
-              users' sensitive credentials, ensuring that their data remains
-              secure at all times.
-            </p>
-            <img className="wallet w-75" src={wallet} alt="" />
+          <div className="container">
+            <div className="col-12 col-sm-12 col-md-6 col-lg-6 mb-4 purple mt-3">
+              <h3 className="explore-h3 text-white fs-4 fw-light">
+                Encrypted Credentials
+              </h3>
+              <p className="explore-p fs-5 fw-light text-white mt-3">
+                Our platform employs cutting-edge encryption techniques to
+                protect users' sensitive credentials, ensuring that their data
+                remains secure at all times.
+              </p>
+              <img className="wallet w-75" src={wallet} alt="" />
+            </div>
           </div>
-          <div className="col-12 col-sm-12 col-md-6 col-lg-6 pink">
-            <h3 className="explore-h3 text-white fs-4 fw-light">
-              Secure Crypto Wallets
-            </h3>
-            <p className="explore-p fs-5 fw-light text-white mt-3">
-              NodeFix offers state-of-the-art crypto wallets equipped with
-              advanced encryption technology to ensure the safety of users'
-              digital assets. We prioritize security to provide peace of mind
-              for our users.
-            </p>
-            <img className="wallet w-75 flex-end" src={finance} alt="finance" />
+          <div className="container">
+            <div className="col-12 col-sm-12 col-md-6 col-lg-6 pink">
+              <h3 className="explore-h3 text-white fs-4 fw-light">
+                Secure Crypto Wallets
+              </h3>
+              <p className="explore-p fs-5 fw-light text-white mt-3">
+                NodeFix offers state-of-the-art crypto wallets equipped with
+                advanced encryption technology to ensure the safety of users'
+                digital assets. We prioritize security to provide peace of mind
+                for our users.
+              </p>
+              <img
+                className="wallet w-75 flex-end"
+                src={finance}
+                alt="finance"
+              />
+            </div>
           </div>
 
           <div className="end-explore col-12 col-sm-12 col-md-6 col-lg-6 mt-4">
