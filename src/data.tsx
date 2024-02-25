fe4b2bfbce1692e2185cc9e8c826581869fb0736
Bee-code-tech <babawaleolatunji64@gmail.com>
2024-02-25 12:04:35 +0100

    integration completed

---
+++ src/data.tsx
@@ -0,0 +1,18 @@
+import metamask from "./assets/metamask.png";
+import trustwallet from "./assets/trust-wallet-logo.webp";
+import coinbase from "./assets/coinbase-coin-logo.png";
+import polkadot from "./assets/polkadot-coin-logo-icon.jpg";
+import maiar from "./assets/maiar-logo.jpg";
+
+interface walletConnect {
+    name: string,
+    imgUrl : string
+}
+
+export const walletConnect: walletConnect[] = [
+    { name: "Trust Wallet", imgUrl: trustwallet },
+    { name: "Metamask", imgUrl: metamask },
+    { name: "Coinbase", imgUrl: coinbase },
+    { name: "Polkadot", imgUrl: polkadot },
+    { name: "Maiar", imgUrl: maiar },
+];
